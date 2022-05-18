6ce4a96193809ece27a45254431872cf47245dcf
Anuar Hernandez <anuarhernandezdelgado@gmail.com>
2022-05-17 19:11:01 -0500

    Pagina de audifonos

--- src/const/index.js
+++
@@ -1,9 +0,0 @@
-export const PAIS = [
-    {id: 'US', nombre: 'Estados Unidos'},
-    {id: 'MX', nombre: 'México'},
-    {id: 'AR', nombre: 'Argentina'},
-    {id: 'CO', nombre: 'Colombia'},
-    {id: 'CR', nombre: 'Costa Rica'},
-    {id: 'ES', nombre: 'España'},
-    {id: 'PE', nombre: 'Perú'},
-]
